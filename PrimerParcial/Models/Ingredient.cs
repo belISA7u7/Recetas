787b91e4f9a1ca2d037e948f193ac215c839d6e6
bellSA7u7 <mbi3006103@est.univalle.edu>
2025-09-25 19:59:21 -0400

    Se modifico el model Category se implemento otro Required y en el modelo Ingredient.cs se borro los comentarios

--- PrimerParcial/Models/Ingredient.cs
+++ PrimerParcial/Models/Ingredient.cs
@@ -4,21 +4,17 @@ namespace PrimerParcial.Models
 {
     public class Ingredient
     {
-        // Clave Primaria (PK)
         public int Id { get; set; }
 
         [Required]
-        public string Name { get; set; } // Ejemplo: "Harina de Trigo"
+        public string Name { get; set; }
 
         [Required]
-        public string Quantity { get; set; } // Ejemplo: "2 tazas" o "500 gramos"
+        public string Quantity { get; set; }
 
-        // --- Relaciones de Entity Framework Core ---
-
-        // Clave Foránea (FK): Vincula este ingrediente a la receta
         public int RecipeId { get; set; }
-            
-        // Propiedad de Navegación: El lado 'uno' kdowjofjf
+
+        // Propiedad de Navegación: El lado 'uno'
         public Recipe Recipe { get; set; }
     }
 }
