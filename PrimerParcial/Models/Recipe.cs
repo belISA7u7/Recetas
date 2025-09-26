4a523b36ac39076e20b60701b596a70acc3e5fd5
bellSA7u7 <mbi3006103@est.univalle.edu>
2025-09-25 20:16:35 -0400

    Se hizo la migracion correctamente

--- PrimerParcial/Models/Recipe.cs
+++ PrimerParcial/Models/Recipe.cs
@@ -1,6 +1,34 @@
-﻿namespace PrimerParcial.Models
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PrimerParcial.Models
 {
     public class Recipe
     {
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; }
+       
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public int PreparationTimeMinutes { get; set; }
+
+        [Required]
+        public int Servings { get; set; }
+
+        [Required]
+        public string Instructions { get; set; }
+
+        public DateTime DateCreated { get; set; } = DateTime.Now;
+
+        // Relaciones
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+        public ICollection<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
     }
 }
